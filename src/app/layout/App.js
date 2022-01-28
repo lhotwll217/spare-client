976bfae36a1b7064b6a75190b0d51960ead59ab6
lhotwll217 <lukehenryotwell@gmail.com>
2022-01-28 14:35:35 -0500

    first commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
