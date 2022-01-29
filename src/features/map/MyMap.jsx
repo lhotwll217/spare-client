bf3ccb5ce6757e19dc5d14af6931eec26ddaae48
lhotwll217 <lukehenryotwell@gmail.com>
2022-01-28 19:40:08 -0500

    EventText max width fixed

--- src/features/map/MyMap.jsx
+++ src/features/map/MyMap.jsx
@@ -4,15 +4,15 @@ export default function MyMap() {
   return (
     <MapContainer
       style={{height: "400px"}}
-      center={[51.505, -0.09]}
-      zoom={13}
+      center={[42.2173, -73.8646]}
+      zoom={10}
       scrollWheelZoom={false}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[42.2173, -73.8646]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
