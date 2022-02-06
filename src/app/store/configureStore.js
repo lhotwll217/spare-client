7508bda87af1a03a225dd5269df3487fbc10e2b6
lhotwll217 <lukehenryotwell@gmail.com>
2022-02-06 13:39:08 -0500

    Auth Persistance

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -2,8 +2,18 @@ import {applyMiddleware, createStore} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import rootReducer from "./rootReducer";
 import thunk from "redux-thunk";
+import {verifyAuth} from "../../features/auth/authActions";
 
 //composeWuthDevTools always us to use more middleware
 export function configureStore() {
-  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+  //Define our store so we can use it before returning it
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+
+  //Set auth state depending on whether we have a Firebase user in local storage
+  store.dispatch(verifyAuth());
+
+  return store;
 }
--- src/features/auth/authActions.js
+++ src/features/auth/authActions.js
@@ -1,4 +1,5 @@
 import {SIGN_IN_USER, SIGN_OUT_USER} from "./authConstants";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
 
 export function signInUser(payload) {
   return {
@@ -10,5 +11,22 @@ export function signInUser(payload) {
 export function signOutUser() {
   return {
     type: SIGN_OUT_USER,
+  };
+}
+//Dispatch this action in the configure store method so we the auth state is listened to continuously
+export function verifyAuth() {
+  //Return a function with dispatch from the store
+  return function (dispatch) {
+    const auth = getAuth();
+    //Adds an onserver for changers to the user's sign-in state, returns a Firebase user object.
+    return onAuthStateChanged(auth, (user) => {
+      //If there is user allready in storage then dispatch a sugn-in action after retrieving the user object from the store.
+      if (user) {
+        console.log(user);
+        dispatch({type: SIGN_IN_USER, payload: user});
+      } else {
+        dispatch(signOutUser());
+      }
+    });
   };
 }
