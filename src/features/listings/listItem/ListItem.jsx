cb07144e2fab788a536f013729fefd7c85518788
lhotwll217 <lukehenryotwell@gmail.com>
2022-02-12 15:42:37 -0500

    Item Modal With Props

--- src/features/listings/listItem/ListItem.jsx
+++ src/features/listings/listItem/ListItem.jsx
@@ -1,23 +1,22 @@
 import {Header, Item, ItemContent, Segment} from "semantic-ui-react";
+import {format} from "date-fns";
 
-export default function ListItem() {
+export default function ListItem({item}) {
   return (
     <Segment>
       <Item.Group>
         <ItemContent>
-          <Header as='h3' content='What' />
+          <Header as='h3' content={item.title} />
         </ItemContent>
         <ItemContent>
-          <Header as='h5' content='Nearest Town' />
+          <Header as='h5' content={item.location.address} />
         </ItemContent>
       </Item.Group>
       <Item.Group>
         <ItemContent>
           <Header sub={true} content='List Details' />
         </ItemContent>
-        <ItemContent>
-          Details for the item and maybe even a picture?
-        </ItemContent>
+        <ItemContent>{item.listDetails}</ItemContent>
         <Item.Image
           size={"tiny"}
           src='https://upload.wikimedia.org/wikipedia/commons/8/89/Tomato_je.jpg'
@@ -27,15 +26,16 @@ export default function ListItem() {
         <ItemContent>
           <Header sub={true} content='Looking for...' />
         </ItemContent>
-        <ItemContent>
-          Details for the item and maybe even a picture?
-        </ItemContent>
+        <ItemContent>{item.tradeDetails}</ItemContent>
       </Item.Group>
       <Item.Group>
         <ItemContent>
           <Header sub={true} content='Time window...' />
         </ItemContent>
-        <ItemContent>Oct 27th - Nov, 8th</ItemContent>
+        <ItemContent>
+          {format(item.availStart, "LLLL do")} -{" "}
+          {format(item.availEnd, "LLLL do")}
+        </ItemContent>
       </Item.Group>
     </Segment>
   );
--- src/features/listings/listItem/ListItemPage.jsx
+++ src/features/listings/listItem/ListItemPage.jsx
@@ -7,12 +7,12 @@ export default function ListDetailedPage(props) {
   console.log(props.item);
   const latLng = {lat: 41.9318294, lng: -73.907437};
   return (
-    <ModalWrapper size='small' width={300} marginTop={40}>
+    <ModalWrapper size='small' width={"70%"} marginTop={40}>
       {" "}
-      <div style={{maxWidth: "300px", margin: "auto"}}>
+      <div style={{margin: "auto"}}>
         {" "}
         <MyMap latLng={latLng} height={"250px"} />
-        <ListItem />
+        <ListItem item={props.item} />
       </div>
     </ModalWrapper>
   );
