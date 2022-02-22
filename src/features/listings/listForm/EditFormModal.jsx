e151de1ca498023881a7079e4c7da38da53fd798
lhotwll217 <lukehenryotwell@gmail.com>
2022-02-22 14:38:01 -0500

    Edit Form Initial Values

--- src/features/listings/listForm/EditFormModal.jsx
+++ src/features/listings/listForm/EditFormModal.jsx
@@ -13,23 +13,14 @@ import MyDateInput from "../../../app/common/form/MyDatePicker";
 import MyPlaceInput from "../../../app/common/form/MyPlaceInput";
 import MyTextArea from "../../../app/common/form/MyTextArea";
 import MyTextInput from "../../../app/common/form/MyTextInput";
-import {useSelector} from "react-redux";
 import {addListing} from "../../../app/firebase/firestoreService";
 import {useNavigate} from "react-router-dom";
 import ModalWrapper from "../../../app/common/modals/ModalWrapper";
 
-export default function EditFormModal() {
-  const user = useSelector((state) => state.auth.currentUser);
+export default function EditFormModal({item}) {
   const navigate = useNavigate();
 
-  const initialValues = {
-    title: "",
-    listDetails: "",
-    tradeDetails: "",
-    availStart: "",
-    availEnd: "",
-    location: {address: "", latLng: ""},
-  };
+  const initialValues = item;
 
   const validationSchema = Yup.object({
     title: Yup.string().required("Required"),
@@ -43,79 +34,66 @@ export default function EditFormModal() {
   });
   return (
     <ModalWrapper width='70%'>
-      <Grid centered>
-        <GridColumn width={12}>
-          <Segment style={{marginTop: "50px"}} clearing>
-            <Formik
-              initialValues={initialValues}
-              onSubmit={async (values, {setErrors, setSubmitting}) => {
-                try {
-                  const docRef = await addListing(values);
-                  console.log(docRef);
-                  setSubmitting(false);
-                  navigate("/");
-                } catch (error) {
-                  setErrors({firestore: error.message});
-                }
-              }}
-              enableReinitialize
-              validationSchema={validationSchema}
-            >
-              {({
-                values,
-                isValid,
-                dirty,
-                isSubmitting,
-                errors,
-                handleSubmit,
-              }) => (
-                <Form onSubmit={handleSubmit}>
-                  <Header content='List Form' />
-                  <MyTextInput placeholder='Title' name='title' />
-                  <MyTextArea
-                    rows={3}
-                    placeholder='Details of listing'
-                    name='listDetails'
-                  />
-                  <MyTextArea
-                    rows={3}
-                    placeholder='Trade for...'
-                    name='tradeDetails'
-                  />
+      <Formik
+        initialValues={initialValues}
+        onSubmit={async (values, {setErrors, setSubmitting}) => {
+          try {
+            const docRef = await addListing(values);
+            console.log(docRef);
+            setSubmitting(false);
+            navigate("/");
+          } catch (error) {
+            setErrors({firestore: error.message});
+          }
+        }}
+        enableReinitialize
+        validationSchema={validationSchema}
+      >
+        {({values, isValid, dirty, isSubmitting, errors, handleSubmit}) => (
+          <Form onSubmit={handleSubmit}>
+            <Header content='Edit Listing' />
+            <MyTextInput placeholder='Title' name='title' />
+            <MyTextArea
+              rows={3}
+              placeholder='Details of listing'
+              name='listDetails'
+            />
+            <MyTextArea
+              rows={3}
+              placeholder='Trade for...'
+              name='tradeDetails'
+            />
 
-                  <MyDateInput
-                    type='date'
-                    name='availStart'
-                    placeholderText='Availability start...'
-                    timeCaption='time'
-                    dateFormat='MMMM d, yyyy '
-                    autoComplete='off'
-                  />
-                  <MyDateInput
-                    name='availEnd'
-                    placeholderText='Availability end...'
-                    timeCaption='time'
-                    dateFormat='MMMM d, yyyy'
-                    autoComplete='off'
-                  />
-                  <MyPlaceInput placeholder='Location...' name='location' />
-                  {errors.firestore && (
-                    <div style={{marginBottom: "10px"}}>
-                      <Label basic color='red' content={errors.firestore} />
-                    </div>
-                  )}
-                  <Button
-                    disabled={!dirty || !isValid || isSubmitting}
-                    type='submit'
-                    content='Submit'
-                    color='teal'
-                  />
-                </Form>
-              )}
-            </Formik>
-          </Segment>
-        </GridColumn>
-      </Grid>
+            <MyDateInput
+              type='date'
+              name='availStart'
+              placeholderText='Availability start...'
+              timeCaption='time'
+              dateFormat='MMMM d, yyyy '
+              autoComplete='off'
+            />
+            <MyDateInput
+              name='availEnd'
+              placeholderText='Availability end...'
+              timeCaption='time'
+              dateFormat='MMMM d, yyyy'
+              autoComplete='off'
+            />
+            <MyPlaceInput placeholder='Location...' name='location' />
+            {errors.firestore && (
+              <div style={{marginBottom: "10px"}}>
+                <Label basic color='red' content={errors.firestore} />
+              </div>
+            )}
+            <Button
+              disabled={!dirty || !isValid || isSubmitting}
+              type='submit'
+              content='Submit'
+              color='teal'
+            />
+          </Form>
+        )}
+      </Formik>
     </ModalWrapper>
   );
 }
