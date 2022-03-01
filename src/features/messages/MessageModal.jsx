354841f2035bc2f908fb32f7648f07e21d02eb9b
lhotwll217 <lukehenryotwell@gmail.com>
2022-03-01 15:48:12 -0800

    Message Modal Content

--- src/features/messages/MessageModal.jsx
+++ src/features/messages/MessageModal.jsx
@@ -1,28 +1,35 @@
 import {
+  Button,
   Item,
   ItemContent,
   ItemDescription,
+  ItemGroup,
   ItemHeader,
   ItemImage,
   ItemMeta,
 } from "semantic-ui-react";
 import ModalWrapper from "../../app/common/modals/ModalWrapper";
+import {formatDistanceToNow} from "date-fns";
 
 export default function MessageModal({message}) {
   return (
     <ModalWrapper>
-      <Item>
-        <ItemImage
-          size='tiny'
-          src='https://react.semantic-ui.com/images/wireframe/image.png'
-        />
-        <ItemContent>
-          <ItemMeta content='sentBy' />
-          <ItemHeader content='message.listing.title' />
-          <ItemDescription content='message.text' />
-          <ItemMeta content='sent time ago' />
-        </ItemContent>
-      </Item>
+      <ItemGroup>
+        <Item>
+          <ItemImage circular size='tiny' src={message.sentBy.photoURL} />
+
+          <ItemContent>
+            <ItemHeader content={message.listing.title} />
+            <ItemDescription content={message.text} />
+            <ItemMeta
+              content={`Sent by ${
+                message.sentBy.displayName
+              } ${formatDistanceToNow(message.date, {addSuffix: true})}`}
+            />
+            <Button content='Reply' size='small' color='teal' />
+          </ItemContent>
+        </Item>
+      </ItemGroup>
     </ModalWrapper>
   );
 }
